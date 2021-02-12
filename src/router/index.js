34ff7d1743a759d67f2728fd73e86ee34cb9d2bb
boushib <boushib01@gmail.com>
2021-02-12 13:28:58 +0100

    add a  base login page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
 import AddVehicle from "@/views/AddVehicle.vue";
+import Login from "@/views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/add-vehicle",
     name: "add_vehicle",
     component: AddVehicle,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
   },
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="login page">
+    <div class="container">
+      <h1>Login</h1>
+    </div>
+  </div>
+</template>

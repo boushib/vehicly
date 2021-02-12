fc868f61f77ee8d125b898dc07451a1207d868da
boushib <boushib01@gmail.com>
2021-02-12 13:27:39 +0100

    create a base add vehicle page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
+import AddVehicle from "@/views/AddVehicle.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/add-vehicle",
+    name: "add_vehicle",
+    component: AddVehicle,
   },
 ];
 
---
+++ src/views/AddVehicle.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="add-vehicle page">
+    <div class="container">
+      <h1>Add a new vehicle</h1>
+    </div>
+  </div>
+</template>

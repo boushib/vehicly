bd9bdb5aed92620ad798b443552277bb19b394e4
boushib <boushib01@gmail.com>
2021-02-15 18:21:10 +0100

    display logout and username in a dropdown

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,10 +13,14 @@
           <li><router-link to="/listings/add">Add Vehicle</router-link></li>
         </ul>
         <div class="user" v-if="isAuth">
-          <div class="username">
-            Logged in as <b>{{ username }}</b>
+          <div class="avatar"></div>
+          <div class="dropdown">
+            <div class="username">
+              Logged in as <b>{{ username }}</b>
+            </div>
+            <div>Profile</div>
+            <div class="logout" @click="logout">Logout</div>
           </div>
-          <div class="logout" @click="logout">Logout</div>
         </div>
         <router-link to="/login" v-else>
           <button class="btn">Login</button>
@@ -98,6 +102,46 @@ nav {
   .user {
     display: flex;
     font-size: 16px;
+    position: relative;
+    cursor: pointer;
+    &:hover {
+      .dropdown {
+        display: block;
+      }
+    }
+    .dropdown {
+      position: absolute;
+      top: 38px;
+      left: -10px;
+      background-color: #fff;
+      width: 200px;
+      padding: 16px;
+      border-radius: 4px;
+      box-shadow: 0 2px 6px rgba(#000, 0.1);
+      display: none;
+      & > div {
+        line-height: 1.8;
+        cursor: pointer;
+      }
+      &::after {
+        content: '';
+        position: absolute;
+        top: -16px;
+        left: 23px;
+        border: 8px solid transparent;
+        border-bottom-color: #fff;
+        z-index: 2;
+      }
+    }
+  }
+  .avatar {
+    width: 40px;
+    height: 40px;
+    background-color: $green;
+    border-radius: 50%;
+    background-image: url('https://www.gravatar.com/avatar/b8e3a666521702b25fc6a4b784d20856?default=https%3A%2F%2Fcloud.digitalocean.com%2Favatars%2Fdefault5.png&secure=true');
+    background-size: cover;
+    background-position: center;
   }
   .username {
     margin-right: 16px;
